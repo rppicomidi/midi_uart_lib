e5f86bec6f1f5338cad9057a5d312fb3c1908402
rppicomidi <rppicomidi@gmail.com>
2022-01-12 15:52:25 -0800

    Update documentation

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# midi_uart_lib
+# midi\_uart\_lib
 Library for using Raspberry Pi Pico UART1 or both UART0 and UART1 for serial port MIDI
+
+Projects that use midi\_uart\_lib must contain a file called `midi_uart_lib_config.h`
+that contains the following code
+
+```
+// Legal values for SERIAL_PORT_MIDI_NUM_UARTS:
+// 1: assigns RP2040 UART1 to MIDI and does not assign UART0
+// 2: assigns both RP2040 UART0 and UART1 to MIDI
+// Any other value will cause a compiler error
+// Note if you use both serial ports for MIDI then neither will
+// be available for debugging
+#define SERIAL_PORT_MIDI_NUM_UARTS 1
+
+```
+
+See the [midi2usbhost](https://github.com/rppicomidi/midi2usbhost) project for an
+example of the `midi_uart_lib_config.h` file and for an example of how the library
+is used.
--- ring_buffer_lib_config.h
+++ ring_buffer_lib_config.h
@@ -33,21 +33,36 @@
 #ifndef RING_BUFFER_LIB_CONFIG
 #define RING_BUFFER_LIB_CONFIG
 
-#include "hardware/irq.h"
+// You can define a larger ring buffer size type if you want
+// RING_BUFFER_SIZE_TYPE has to be able to hold a value less
+// than or equal to the maximum number of bytes in a ring buffer
+// Uncomment and modify the next line if the default (uint8_t)
+// is not big enough
+// #define RING_BUFFER_SIZE_TYPE uint8_t
 
-#ifdef RING_BUFFER_ENTER_CRITICAL
-#undef RING_BUFFER_ENTER_CRITICAL
-#endif
+// If you don't need to support multiple cores modifying the
+// ring buffer, set RING_BUFFER_MULTICORE_SUPPORT to 0; it
+// will save time and space
+// Otherwise set it to 1. Uncomment the next line to enable
+// multicore support
+// #define RING_BUFFER_MULTICORE_SUPPORT 1
+
+// If RING_BUFFER_MULTICORE_SUPPORT is 0, then safe version
+// of ring buffer calls by default disable all IRQs on the
+// current core, manipulate the buffer, and then restore IRQs.
+// That is probably overkill for most applications. For
+// example, if you are using this ring buffer with a peripheral
+// driver, you can store the IRQ number to disable in the
+// critical_section_data field of the ring_buffer structure
+// in the ring_buffer_init() function call and then use
+// that value to enable or disable only that peripheral IRQ.
+// Uncomment the example below to do that
+#include "hardware/irq.h"
 
 #define RING_BUFFER_ENTER_CRITICAL(X) \
     do {irq_set_enabled(ring_buf->critical_section_data, false);} while (0)
 
-#ifdef RING_BUFFER_EXIT_CRITICAL
-#undef RING_BUFFER_EXIT_CRITICAL
-#endif
-
 #define RING_BUFFER_EXIT_CRITICAL(X) \
     do {irq_set_enabled(ring_buf->critical_section_data, true);} while (0)
-
 
 #endif
